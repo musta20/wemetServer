62385c09760646c7bdabebc7df193d8937bbed7b
musta20 <must.g20@gmail.com>
2024-09-25 03:40:21 +0300

    add test deploy

--- Server.js
+++ Server.js
@@ -108,10 +108,10 @@ async function startExpressServer() {
     }
   });
 
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "dist")));
 
   app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
   });
 }
 
